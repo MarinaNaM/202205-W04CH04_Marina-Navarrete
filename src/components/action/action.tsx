232c8903c5ef369bda89ba163733f620aeebc802
Marina Navarrete <mnavarretem@gmail.com>
2022-06-02 20:44:52 +0200

    Update actions file

--- src/components/action/action.tsx
+++ src/components/action/action.tsx
@@ -1,7 +1,13 @@
+import React from 'react';
 export function Action() {
     return (
-        <a href="#" className="call">
-            Call
-        </a>
+        <>
+            <a href="#" className="call">
+                Call
+            </a>
+            <a href="#" className="hang active">
+                Hang
+            </a>
+        </>
     );
 }
