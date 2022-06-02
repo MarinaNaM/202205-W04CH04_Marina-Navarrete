8e54f75c26f06c40d38fff06fd034669e4e213ed
Marina Navarrete <mnavarretem@gmail.com>
2022-06-02 19:49:27 +0200

    Add numbers print

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Keyboard } from '../Keyboard';
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Keyboard></Keyboard>
+        </div>
+    );
 }
 
 export default App;
